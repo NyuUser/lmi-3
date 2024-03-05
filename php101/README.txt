112a68dfad968ee97634e59caa95e81592308434
Robert Vien L. Santiago <rvlsantiago25@gmail.com>
2024-03-05 14:13:58 +0800

    added employeedb dump file

--- php101/README.txt
+++ php101/README.txt
@@ -2,7 +2,9 @@ html --> http://localhost/lmi-3/php101/html.html#
 javascript --> http://localhost/lmi-3/php101/javascript.html#
 mysql commands --> http://localhost/lmi-3/php101/mysql.php
 
-import database content --> http://localhost/lmi-3/php101/import_dump_file.php
+import database content 
+employeedb --> http://localhost/lmi-3/php101/import_employeedb_dump.php
+new_emp --> http://localhost/lmi-3/php101/import_new_emp_dump.php
 
 before importing 
  - create database name new_emp
---
+++ php101/import_employeedb_dump.php
@@ -0,0 +1,29 @@
+<?php
+// Database connection details
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "employeedb";
+
+// MySQL dump file path
+$dumpFilePath = "C:/xampp/htdocs/lmi-3/php101/employeedb.sql";
+// Create a new mysqli connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Read the SQL dump file content
+$sql = file_get_contents($dumpFilePath);
+
+// Execute the SQL commands
+if ($conn->multi_query($sql)) {
+    echo "Import successful.";
+} else {
+    echo "Error importing dump file: " . $conn->error;
+}
+
+// Close the connection
+$conn->close();
